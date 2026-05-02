495c3d0138c84f5086973acc8c2c1781216efcda
Eric Bush <eric.bush@mailbox.tu-dresden.de>
2026-05-02 22:46:12 +0200

    Add cmsis

--- Src/main.c
+++ Src/main.c
@@ -35,7 +35,7 @@ int main(void)
 
     vStartTasks();
 
-    // vTaskStartScheduler();
+    vTaskStartScheduler();
 
 	for(;;){}
 }
@@ -97,4 +97,17 @@ static void vWriteSPITask( void * pvParameters )
     for( ; ; )
     {
     }
+}
+
+void vApplicationIdleHook( void )
+{
+    /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+     * to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+     * task.  It is essential that code added to this hook function never attempts
+     * to block in any way (for example, call xQueueReceive() with a block time
+     * specified, or call vTaskDelay()).  If the application makes use of the
+     * vTaskDelete() API function (as this demo application does) then it is also
+     * important that vApplicationIdleHook() is permitted to return to its calling
+     * function, because it is the responsibility of the idle task to clean up
+     * memory allocated by the kernel to any task that has since been deleted. */
 }
