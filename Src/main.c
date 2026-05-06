e96d2b07f59df22f91fac5afe4d4148adc3c2ec1
Eric Bush <eric.bush@mailbox.tu-dresden.de>
2026-05-07 01:56:32 +0200

    Configure system clock

--- Src/main.c
+++ Src/main.c
@@ -23,9 +23,11 @@
 #include "task.h"
 
 #include "stm32l4xx_hal.h"
+#include "stm32l4xx_hal_rcc.h"
 
 static void prvSetupHardware( void );
 static void vStartTasks( void );
+static void SystemClock_Config( void );
 
 static void vReadUARTTask( void * pvParameters );
 static void vReadI2CTask( void * pvParameters );
@@ -46,16 +48,48 @@ static void prvSetupHardware( void )
 {
     HAL_Init();
 
-    /* Ensure all priority bits are assigned as preemption priority bits. */
-    // NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
+    SystemClock_Config();
 
-    /* Setup the LED outputs. */
-    // vParTestInitialise();
+    SystemCoreClockUpdate();
 
-    /* Configure the button input.  This configures the interrupt to use the
-     * lowest interrupt priority, so it is ok to use the ISR safe FreeRTOS API
-     * from the button interrupt handler. */
-    // STM_EVAL_PBInit( BUTTON_USER, BUTTON_MODE_EXTI );
+    HAL_NVIC_SetPriorityGrouping( NVIC_PRIORITYGROUP_4 );
+}
+
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+
+  /* MSI is enabled after System reset, activate PLL with MSI as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 40;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLP = 7;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
+  
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
 }
 
 static void vStartTasks( void )
