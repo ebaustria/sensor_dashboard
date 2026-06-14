0b166d599920cfdb4b93d1d5201487a350830b87
Eric Bush <eric.bush@mailbox.tu-dresden.de>
2026-06-14 22:12:06 +0200

    Formatting

--- lib/OLED/oled.h
+++ lib/OLED/oled.h
@@ -6,8 +6,8 @@ extern "C"
 {
 #endif
 
-#include "u8x8.h"
 #include "minmea.h"
+#include "u8x8.h"
 #include <stdint.h>
 
     uint8_t u8x8_gpio_and_delay_stm32(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
